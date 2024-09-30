0e326676d01ed358bc38ca27f96b1347da050b3f
Hamsapriya J L <110473412+urstrulyhamsa@users.noreply.github.com>
2024-09-30 11:43:21 +0530

    Create consumer.py

---
+++ consumer.py
@@ -0,0 +1,19 @@
+import mysql.connector
+from kafka import KafkaConsumer
+import json
+
+# Connect to MySQL
+db = mysql.connector.connect(user='your_user', password='your_password', host='localhost', database='finCorp')
+cursor = db.cursor()
+
+# Initialize the Kafka consumer
+consumer = KafkaConsumer('stock_topic',
+                         bootstrap_servers='localhost:9092',
+                         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
+for message in consumer:
+    stock_data = message.value
+    # Insert into MySQL
+    cursor.execute("INSERT INTO stock_data (index_name, timestamp, stock_price, trading_volume) VALUES (%s, %s, %s, %s)",
+                   (stock_data['index_name'], stock_data['timestamp'], stock_data['stock_price'], stock_data['trading_volume']))
+    db.commit()

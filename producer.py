3bf8ead43ff52897420ccc6db0b7f07ce9412a37
Hamsapriya J L <110473412+urstrulyhamsa@users.noreply.github.com>
2024-09-30 11:17:37 +0530

    Create producer.py

---
+++ producer.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from kafka import KafkaProducer
+import json
+
+# Initialize the Kafka producer
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+# Load stock data
+data = pd.read_csv('C:\Users\Admin\Downloads\stock_data_new.csv')
+
+# Send data to Kafka topic
+for index, row in data.iterrows():
+    producer.send('stock_topic', value=row.to_dict())
+producer.flush()

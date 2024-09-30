823fc750ac8cc89187b1814b4a7e9f2b22dc9256
Hamsapriya J L <110473412+urstrulyhamsa@users.noreply.github.com>
2024-09-30 03:08:06 -0700

    Update producer.py

--- producer.py
+++ producer.py
@@ -7,7 +7,7 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 # Load stock data
-data = pd.read_csv('C:\Users\Admin\Downloads\stock_data_new.csv')
+data = pd.read_csv("C:\Users\Admin\Downloads\stock_data_new.csv")
 
 # Send data to Kafka topic
 for index, row in data.iterrows():
